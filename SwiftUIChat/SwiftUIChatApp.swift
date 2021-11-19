7031c0dfec2ad13a090279bb8480643ce54fa99c
Salma Hassan <salmahassan087@gmail.com>
2021-11-19 19:20:57 +0200

    Initial Commit

---
+++ SwiftUIChat/SwiftUIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChatApp.swift
+//  SwiftUIChat
+//
+//  Created by Salma Hassan on 19/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
